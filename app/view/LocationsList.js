515c58d20844b75b811bd43188f7ab5267f6a743
invercity <invercity1991@gmail.com>
2014-04-14 21:31:26 +0300

    final changes

--- app/view/LocationsList.js
+++ app/view/LocationsList.js
@@ -2,6 +2,7 @@
     extend: "Ext.dataview.List",
     alias: "widget.locationslist",
     config: {
+        loadingText: 'Please wait...',
         emptyText: "<div class=\"notes-list-empty-text\">No locations found.</div>",
         onItemDisclosure: true,
         itemTpl: "<div class=\"list-item-title\">{name}</div><div class=\"list-item-narrative\">{country}</div>"
